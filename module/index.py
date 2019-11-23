b16c681dc29f39332ec31c61c6a466dca05c9b0d
Julien <julien.fradet5@gmail.com>
2019-11-23 18:04:13 -0500

    Handling errors and exceptions improved

--- module/index.py
+++ module/index.py
@@ -1,6 +1,20 @@
+'''
+FOREX Transactions
+'''
 import csv
+import os
 import click
 import pycountry
+
+class FileError(Exception):
+    '''
+    Exception for errors due too currency pairs or codes
+    '''
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
 
 class CurrencyPairs:
     '''
@@ -26,10 +40,13 @@ def is_currency_code(currency_codes):
     currency_code_list = []
     for item in list(pycountry.currencies):
         currency_code_list.append(str(item).split(',')[0][-4:-1])
-    for currency_code in currency_codes:
-        if currency_code not in currency_code_list:
-            print("Insert correct currency codes inside your csv file please.")
-            return False
+    try:
+        for currency_code in currency_codes:
+            if currency_code not in currency_code_list:
+                raise FileError("Insert correct currency codes inside your csv file please.")
+    except FileError as error:
+        print(error)
+        return False
     return True
 
 def find_currency_pair(currency_pair):
@@ -38,43 +55,49 @@ def find_currency_pair(currency_pair):
     '''
     if currency_pair == 'pair':
         return True
-    else:
-        try:
-            currency_codes = currency_pair.split('/')
-            return is_currency_code(currency_codes)
-        except:
-            print("Insert correct currency codes inside your csv file please.")
-    return False
+    try:
+        slash = currency_pair.find('/')
+        if slash == -1:
+            raise FileError("Insert correct currency pairs inside your csv file please.")
+        currency_codes = currency_pair.split('/')
+    except FileError as error:
+        print(error)
+        return False
+    return is_currency_code(currency_codes)
 
 def clean_row(row):
     '''
     Clean each row of any whitespace
     '''
     new_row = []
-    for item in row:
-        item = item.strip()
-        new_row.append(item)
-    del row
+    try:
+        for item in row:
+            item = item.strip()
+            if len(item) == 0:
+                raise FileError("One currency pair is empty inside your csv.")
+            new_row.append(item)
+    except FileError as error:
+        print(error)
+        return False
+    finally:
+        del row
     return new_row
 
 def is_csv(file):
     '''
     Check if the file received in parameter is a correct CSV file
     '''
-    if not file.endswith('.csv'):
+    if not file.endswith('.csv') or os.path.getsize(file) <= 0:
         print("Insert a correct CSV file please.")
         return False
-    try:
-        with open(file, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
-            for row in reader:
-                row = clean_row(row)
-                if not find_currency_pair(row[2]):
-                    return False
-            return True
-    except:
-        print("Insert a correct CSV file please.")
-        return False
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            row = clean_row(row)
+            if not row or not find_currency_pair(row[2]):
+                return False
+        del reader
+    return True
 
 @click.group()
 def cli():
